b939ca7334d7eb755c1c5d90be366fba1c295a08
DerpyFirework <epik1337pwner@gmail.com>
2017-11-14 03:27:02 +0000

    Initial commit
    Currently a quick hacked together prototype. Will expand on this for use
    in my next career save while testing in ongoing career save.
    Public KSP forums release highly unlikely

---
+++ TransactionTracker/Tracker.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using UnityEngine;
+using System.IO;
+
+namespace TransactionTracker
+{
+    [KSPAddon(KSPAddon.Startup.MainMenu, true)]
+    public class Tracker : MonoBehaviour
+    {
+        string filePath
+        {
+            get
+            {
+                return KSPUtil.ApplicationRootPath + "/saves/" + HighLogic.SaveFolder + "/";
+            }
+        }
+        public void Start()
+        {
+            GameEvents.OnFundsChanged.Add(OnFundsChanged);
+            GameEvents.OnScienceChanged.Add(OnScienceChanged);
+            GameEvents.OnReputationChanged.Add(OnReputationChanged);
+        }
+
+        public void OnFundsChanged(double amount, TransactionReasons reason)
+        {
+            string output = string.Format("{0},{1},{2}",HighLogic.CurrentGame.UniversalTime, amount, reason);
+            string fileName = "funds.csv";
+            File.AppendAllText(filePath + fileName, output+System.Environment.NewLine);
+        }
+
+        public void OnScienceChanged(float amount, TransactionReasons reason)
+        {
+            string output = string.Format("{0},{1},{2}", HighLogic.CurrentGame.UniversalTime, amount, reason);
+            string fileName = "science.csv";
+            File.AppendAllText(filePath + fileName, output + System.Environment.NewLine);
+        }
+
+        public void OnReputationChanged(float amount, TransactionReasons reason)
+        {
+            string output = string.Format("{0},{1},{2}", HighLogic.CurrentGame.UniversalTime, amount, reason);
+            string fileName = "reputation.csv";
+            File.AppendAllText(filePath + fileName, output + System.Environment.NewLine);
+        }
+    }
+}
